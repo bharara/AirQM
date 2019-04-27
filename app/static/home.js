e0aa0cffffd50b53dbfff3b89f1d4dba14200086
bharara <32545366+bharara@users.noreply.github.com>
2019-04-27 20:55:10 +0500

    Initial commit

---
+++ app/static/home.js
@@ -0,0 +1,8 @@
+var toggle_button = document.querySelector("#toggle_button");
+var radius = document.querySelector(".navbar-inverse");
+var jumb = document.querySelector("#j2");
+toggle_button.addEventListener("click",function(){
+	radius.classList.toggle("radius1");
+
+});
+
---
+++ run.py
@@ -0,0 +1,7 @@
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+## let marker = createMarker({lat: 33.684, lng: 73.0479}, "m1", "red");
